412591c01ead79f367da30f74de463d606379801
Vannesha <112999663+Vannesha05@users.noreply.github.com>
2022-09-07 04:41:37 +0000

    variable java

---
+++ Variable.java
@@ -0,0 +1,23 @@
+class Variable{
+  public static void main(String[] args){
+
+    String nama = "Vannesha";
+
+    int angka = 10;
+
+    String angka2 = "5";
+    
+    System.out.println("Selamat pagi "+nama);
+
+    System.out.println(nama + angka);
+    
+    System.out.println("Operasi matematika dalam int");
+    System.out.println(12 + angka);
+
+    System.out.println("Operasi matematika dalam int dan String");
+    System.out.println(angka + angka2);
+    
+    
+  }
+}
+ 
